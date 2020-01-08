bdabaa99d0077a91235df977208c0821df7d9997
trungnguyen1618033 <52783197+trungnguyen1618033@users.noreply.github.com>
2020-01-08 20:05:58 +0700

    Update page

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function close_display() {
+    var x = document.getElementsByClassName('btn-close');
+    console.log(test)
+    if(x.style.display != "block")
+        x.style.display = "none";
+}
