5322216becc9736fb7ff611b051a77d5e75db095
trungnguyen1618033 <52783197+trungnguyen1618033@users.noreply.github.com>
2020-01-11 15:59:08 +0700

    Update lasttime

--- assets/js/page-4.js
+++ assets/js/page-4.js
@@ -1,20 +1,40 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
+// (function () {
+//     'use strict'
   
-    window.addEventListener('load', function () {
-      // Fetch all the forms we want to apply custom Bootstrap validation styles to
-      var forms = document.getElementsByClassName('needs-validation')
+//     window.addEventListener('load', function () {
+
+
+//       document.querySelector('#mot').addEventListener('onclick', function(event) {
+//         event.preventDefault();
+//         console.log('asdasdasd');      })
+
+//       // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//       var forms = document.getElementsByClassName('needs-validation')
   
-      // Loop over them and prevent submission
-      Array.prototype.filter.call(forms, function (form) {
-        form.addEventListener('submit', function (event) {
-          if (form.checkValidity() === false) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-          form.classList.add('was-validated')
-        }, false)
-      })
-    }, false)
-  }())
+//       // Loop over them and prevent submission
+//       // Array.prototype.filter.call(forms, function (form) {
+//       //   form.addEventListener('submit', function (event) {
+//       //     if (form.checkValidity() === false) {
+//       //       event.preventDefault()
+//       //       event.stopPropagation()
+//       //     }
+//       //     form.classList.add('was-validated')
+//       //   }, false)
+//       // })
+//     }, false)
+//   }())
+
+window.addEventListener('load', function() {
+  console.log('asdasd');
+  console.log(document.getElementById('mot'));
+  document.getElementById('mot').addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log('dayne');
+  });
+})
+
+document.getElementById('mot').addEventListener('onclick', function(event) {
+  event.preventDefault();
+  console.log('dayne');
+});
